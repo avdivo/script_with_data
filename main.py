19c92408716925661bff80ba7da66e70353bea06
avdivo <avdivo@mail.ru>
2023-03-13 18:45:03 +0300

    Описание, интерфейс

--- main.py
+++ main.py
@@ -13,24 +13,53 @@ https://metanit.com/python/tkinter/4.1.php
 
 """
 
+#
+# from tkinter import *
+#
+#
+# # Интерфейс
+# root = Tk()
+#
+# # Размер экране
+# w = root.winfo_screenwidth()
+# h = root.winfo_screenheight()
+#
+# # Размер окна
+# win_w = 700
+# win_h = 600
+#
+# root.title("Редактор скриптов")
+# root.geometry(f'{win_w}x{win_h}+{(w-win_w)//2}+{(h-win_h)//2}')  # Рисуем окно
 
-from tkinter import *
+
+import tkinter as tk
+from tkinter import ttk
+
+# def select():
+#     for i in tree.selection():
+#         item_iid = i
+#         print ("".join([str(tree.item(i)['text'])])# for i in curItems]))
 
 
-# Интерфейс
-root = Tk()
+import tkinter as tk
+from tkinter import ttk
 
-# Размер экране
-w = root.winfo_screenwidth()
-h = root.winfo_screenheight()
+root = tk.Tk()
 
-# Размер окна
-win_w = 700
-win_h = 600
+tree = ttk.Treeview(root, show="headings", columns=('size', 'modified'), selectmode='browse')
 
-root.title("Редактор скриптов")
-root.geometry(f'{win_w}x{win_h}+{(w-win_w)//2}+{(h-win_h)//2}')  # Рисуем окно
-print(w, h)
+tree.heading('size', text='SIZE')
+tree.heading('modified', text='MODIFIED')
 
+tree.insert('', 0, 'gallery1', text='Applications1')
+tree.insert('gallery1', 0, 'gallery3', text='Applications1')
+tree.insert('gallery1', 0, 'gallery4', text='Applications1')
 
+tree.insert('', 1, 'gallery2', text='Applications2')
+
+tree.selection_set('gallery1')
+
+tree.focus_set()
+
+tree.grid()
 root.mainloop()
