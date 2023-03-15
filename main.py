d42677c75028c0b44ed11df46c5034d94312fcac
avdivo <avdivo@mail.ru>
2023-03-15 18:04:26 +0300

    Интерфейс

--- main.py
+++ main.py
@@ -31,9 +31,32 @@ win_h = 600
 root.title("Редактор скриптов")
 root.geometry(f'{win_w}x{win_h}+{(w-win_w)//2}+{(h-win_h)//2}')  # Рисуем окно
 
+# Меню
+mainmenu = Menu(root)
+root.config(menu=mainmenu)
+
+filemenu = Menu(mainmenu, tearoff=0)
+filemenu.add_command(label="Новый скрипт")
+filemenu.add_command(label="Открыть скрипт")
+filemenu.add_command(label="Сохранить скрипт")
+filemenu.add_command(label="Сохранить скрипт как...")
+filemenu.add_separator()
+filemenu.add_command(label="Источник данных")
+filemenu.add_separator()
+filemenu.add_command(label="Выход")
+
+mainmenu.add_cascade(label="Файл",
+                     menu=filemenu)
+
+mainmenu.add_cascade(label="Настройки скрипта")
+
+
+# Список команд скрипта
+
 # Create an instance of Style widget
 style=ttk.Style()
 style.theme_use('clam')
+
 
 tree = ttk.Treeview(root, show="", columns=('size', 'modified'), selectmode="extended", height=28)
 tree.column("# 1", stretch=NO, width=80)
@@ -51,5 +74,18 @@ tree.insert('', 0, 'gallery0', values=('', ''))
 tree.selection_set('gallery1')
 tree.focus_set()
 tree.place(x=0, y=0)
+
+INDENT = 400  # Отступ второй колонки от левого края окна
+
+# Источник данных
+frame = Frame(root, width=100, height=100, border=2)
+frame.place(x=INDENT, y=0)
+la = Label(frame, text='Источник данных', anchor="w")
+la.place(x=0, y=0)
+# la.place(anchor=NW)
+# frame.place(x=INDENT, y=0)
+# data_source = Message(root, text='Источник данных', width=390, anchor='w', relief=SUNKEN)
+# data_source.place(x=INDENT, y=30)
+# data_source['text'] = 'Раз два три Раз два три Раз два три Раз два три Раз два три Раз два три Раз два три Раз два три '
 
 root.mainloop()
