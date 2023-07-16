4ba68434ac18b90c1cddc3a0931b8ab8b3fbc11b
avdovo <avdivo@mail.ru>
2023-07-16 13:47:54 +0300

    лор

--- element_images.py
+++ element_images.py
@@ -87,7 +87,7 @@ def compare_2_images(small, big):
 
     return False
 
-#  h_image(x_point: int,    y_poin
+#  h_image(x_point: int,    y_poinоо
     """ Сохранение изображения кнопки/иконки (элемента) если он еще не сохранен
 
     Функция принимает в качестве аргументов координаты точки на экране.
